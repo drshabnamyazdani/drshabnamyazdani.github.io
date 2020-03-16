2c03b169dd04a8544e9798898c28accf4757c640
Ali <alphajafarabadi@gmail.com>
2020-03-16 14:28:25 +0330

    Make it clean and a little bit blue

--- scripts/common.js
+++ scripts/common.js
@@ -4,14 +4,8 @@ $(document).ready(function() {
     var headerOverlay = $(".header__overlay"),
         menuOpenIcon = $(".nav__icon-menu"),
         menuCloseIcon = $(".nav-icon__close"),
-        menuList = $(".main-nav"),
-        searchOpenIcon = $(".nav__icon-search"),
-        searchCloseIcon = $(".search__close"),
-        searchBox = $(".search");
+        menuList = $(".main-nav");
 
-    /* =======================
-    // Menu and Search
-    ======================= */
     menuOpenIcon.click(function() {
         menuOpen();
     })
@@ -20,14 +14,8 @@ $(document).ready(function() {
         menuClose();
     })
 
-    searchOpenIcon.click(function() {
-        searchOpen();
-    });
-
-
     headerOverlay.click(function() {
         menuClose();
-        searchClose();
     });
 
     function menuOpen() {
@@ -39,18 +27,4 @@ $(document).ready(function() {
         menuList.removeClass("is-open");
         headerOverlay.removeClass("is-visible");
     }
-
-    /* =======================
-    // Responsive Videos
-    ======================= */
-    $(".post__content, .page__content").fitVids({
-        customSelector: ['iframe[src*="ted.com"]']
-    });
-
-    /* =======================
-    // Zoom Image
-    ======================= */
-    $(".page img, .post img").attr("data-action", "zoom");
-    $(".page a img, .post a img").removeAttr("data-action", "zoom");
-
 });
